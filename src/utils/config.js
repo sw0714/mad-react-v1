7d7c95fe30481671ebcf5ee734d0c2696566f9f7
sunny0714 <sun2020victory@gmail.com>
2022-03-16 22:51:28 -0800

    tokenlist

---
+++ src/utils/config.js
@@ -0,0 +1,14 @@
+const config = {
+  ethChainId: 1,
+  ethChainIdRinkeby: 4,
+  bscChain: 56,
+  bscChainTestent: 97,
+  hmyChainMainnet: 1666600000,
+  hmyChainTestnet: 1666700000,
+  polygon_chain_mainnet: 137,
+  polygon_chain_testnet: 80001,
+  ankrEthereumRpc: "https://rpc.ankr.com/eth",
+  ankrMoonRiverRpc: "",
+};
+
+export default config;
