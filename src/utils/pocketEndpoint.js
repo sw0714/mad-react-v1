9dde414dc9957bd293e3053de02ff68840a12931
sunny0714 <sun2020victory@gmail.com>
2022-03-16 23:21:52 -0800

    add rpcUrl and endpoints

---
+++ src/utils/pocketEndpoint.js
@@ -0,0 +1,61 @@
+export const ethMainnet = "https://eth-mainnet.gateway.pokt.network/v1/lb/6230d162b2feb200398370c6";
+export const ethRinkeby = "https://eth-rinkeby.gateway.pokt.network/v1/lb/6230d162b2feb200398370c6";
+export const hmyMainnet = "https://harmony-0.gateway.pokt.network/v1/lb/6230d162b2feb200398370c6";
+export const portalID = "6230d162b2feb200398370c6";
+
+export const poktEth = {
+  rpcUrl : 'https://eth-rpc.gateway.pokt.network',
+  chainId : '1',
+  symbol : 'ETH',
+  explorerUrl : 'https://etherscan.io'
+};
+
+export const poktAvalanche = {
+  rpcUrl : 'https://avax-mainnet.gateway.pokt.network/v1/lb/605238bf6b986eea7cf36d5e/ext/bc/C/rpc',
+  chainId : '0xa86a',
+  symbol : 'AVAX',
+  explorerUrl : 'https://cchain.explorer.avax.network/'
+};
+
+export const poktXDai = {
+  rpcUrl : 'https://xdai-rpc.gateway.pokt.network',
+  chainId : '0x64',
+  symbol : 'xDAI',
+  explorerUrl : 'https://blockscout.com/poa/xdai'
+};
+
+export const poktFuse = {
+  rpcUrl : 'https://fuse-rpc.gateway.pokt.network/',
+  chainId : '0x7a',
+  symbol : 'Fuse',
+  explorerUrl : 'https://explorer.fuse.io'
+};
+
+export const poktPolygon = {
+  rpcUrl : 'https://poly-rpc.gateway.pokt.network/',
+  chainId : '137',
+  symbol : 'Matic',
+  explorerUrl : 'https://polygonscan.com'
+};
+
+export const poktBsc = {
+  rpcUrl : 'https://bsc-mainnet.gateway.pokt.network/v1/lb/6136201a7bad1500343e248d',
+  chainId : '56',
+  symbol : 'BNB',
+  explorerUrl : 'https://bscscan.com'
+};
+
+export const poktHarmony = {
+  rpcUrl : 'https://harmony-0-rpc.gateway.pokt.network',
+  chainId : '1666600000',
+  symbol : 'ONE',
+  explorerUrl : 'https://explorer.harmony.one'
+};
+
+export const poktEVMos = {
+  rpcUrl : 'https://evmos-testnet.gateway.pokt.network/v1/lb/61aabb3495d548003aebfd1c',
+  chainId : '9000',
+  symbol : 'PHOTON',
+  explorerUrl : 'https://evm.evmos.org/'
+};
+
