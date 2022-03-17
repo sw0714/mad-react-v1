2db5a0aec8ece653cc831278651998bdb8413b1e
sunny0714 <sun2020victory@gmail.com>
2022-03-16 22:50:17 -0800

    app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Web3ReactProvider} from '@web3-react/core'
+import { ExternalProvider, Web3Provider } from '@ethersproject/providers'
 import './App.css';
+
+const getLibrary = (provider: ExternalProvider) => {
+  return new Web3Provider(provider);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+        <div className="App">
+          <header className="App-header">
+            Welcome to Mad NFT
+          </header>
+        </div>
+      </Web3ReactProvider>
   );
 }
 
